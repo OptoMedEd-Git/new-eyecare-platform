2b2a14b1a854a324a86c93acfc894c3ed25adb6d
OptoMedEd <admin@optomeded.com>
2026-04-18 14:20:27 -0400

    Remove flowbite plugin wrapper causing production 404

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+/* import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+// };
+
+//export default withFlowbiteReact(nextConfig); */
+
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withFlowbiteReact(nextConfig);
+export default nextConfig;
